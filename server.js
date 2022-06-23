03765d8b522851abc4ce357b2c9ec20b22337042
Alice Silva <alicessilva@aol.com>
2022-06-23 19:05:53 -0400

    delete

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const app = express();
+const { v4: uuidv4 } = require('uuid')
 
 const PORT = process.env.PORT || 3001;
-
-const app = express();
 
 const notesData = require('./db/db.json')
 
@@ -13,25 +13,40 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, 'public/db/index.html'))
+    res.sendFile(path.join(__dirname, 'public/index.html'))
 );
 
 app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, 'public/db/notes1.html'))
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 
 app.get('/api/notes', (req, res) => {
-
-    notesData.push(req.body);
-
-    fs.writeFile('./db/db.json', JSON.stringify(notesData), (err) => {
-        err ? res.json(err) : res.send('saved');
-    })
-});
-
-
-app.post('/api/notes', (req, res) => {
     res.json(notesData)
 });
 
-app.listen(PORT);
+app.post('/api/notes', (req, res) => {
+    let newNote = {
+        title: req.body.title,
+        text: req.body.text,
+        id: uuidv4()
+    }
+    notesData.push(newNote)
+    fs.writeFile('./db/db.json', JSON.stringify(notesData), err => {
+        if (err) throw err;
+        console.log('Saved Note!');
+    })
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+    let newNotes = notesData.filter(note => note.id !== req.params.id)
+    fs.writeFile('./db/db.json', JSON.stringify(newNotes), err => {
+        if (err) throw err;
+        console.log('Deleted note!');
+    })
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+});
+
+app.listen(PORT, () => {
+    console.log('listening');
+});
