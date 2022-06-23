86ba25d6d4848442a6c30e88cf552d2fc0d32f0e
Alice Silva <alicessilva@aol.com>
2022-06-22 20:28:50 -0400

    notes

--- server.js
+++ server.js
@@ -4,24 +4,24 @@ const path = require('path');
 const app = express();
 const PORT = 3001;
 
-const notes = require('./db/db.json')
+const notesData = require('./db/db.json')
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.send('public/db/index.html'));
 
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/db/notes.html'))
+app.get('/notes', (req, res) =>
+res.sendFile(path.join(__dirname, 'public/db/notes.html'))
 );
 
+app.get('/', (req, res) => res.send('public/db/index.html'));
 
 app.get('/api/notes/db.json', (req, res) => {
-    res.json(notes)
+    res.json(notesData)
     });
 
 
   app.post('/api/notes/db.json', (req, res) => {
-    res.json(notes)
+    res.json(notesData)
     });
 
 app.listen(PORT, () =>
